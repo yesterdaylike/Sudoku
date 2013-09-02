748e8709735b5a15dc103258e4ec352e7ce9240c
zhengwenhui <zhengwenhui@outlook.com>
2013-09-02 17:16:23 +0800

    init

---
+++ src/com/yesterdaylike/sudoku/PlayActivity9.java
@@ -0,0 +1,484 @@
+package com.yesterdaylike.sudoku;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+import android.app.Activity;
+import android.app.Dialog;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import android.util.Log;
+import android.view.Display;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RatingBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.yesterdaylike.sudoku.DataBaseHelper.Sudoku;
+import com.yesterdaylike.sudoku.PlayPagerAdapter.OnInstantiateItemListener;
+
+/**
+ * @author zheng
+ */
+public class PlayActivity9 extends Activity
+implements OnPageChangeListener, OnTouchListener, OnInstantiateItemListener{
+	private static final int squareheight = 3;
+	private static final int squarewidth = 3;
+	private static final int squarelength = 9;
+	private static final int count = 81;
+
+	private MyViewPager mViewPager;
+	private PlayPagerAdapter mAdapter;
+	private View titleBarLayout;
+	private View downBarLayout;
+	private Panel helpPanel;
+	private RatingBar ratingBar;
+	private TextView sudokuId;
+	private View mClickView;
+	
+	private TextView timerTextView;
+	private TextView closingTimeTextView;
+	
+	private int mClickNumber;
+	private int width;
+	private int height;
+
+	private boolean mPlaying = false;
+	private Sudoku[] mSudokus;
+	private DataBaseHelper mDbHelper;
+	private List<View> mSudokuViewList;
+	private List<TextView[]> mSquareViewList;
+	private int []mSquareViewIds;
+
+	private TextView []mSquaresViews;
+	private int mCurrentIndex = 0;
+	private int[] mData;
+
+	private Dialog mDialog;
+	private int []mDialogViewIds;
+	private TextView []mDialogViews;
+	
+	private int recLen = 0;
+ 
+    Handler handler = new Handler();
+    Runnable runnable = new Runnable() {
+        public void run() {
+            recLen++;
+            //txtView.setText("" + recLen);
+            timerTextView.setText("" + recLen);
+            handler.postDelayed(this, 1000);
+        }
+    };
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_play);
+
+		mDbHelper = new DataBaseHelper(this);
+		mSudokus = mDbHelper.querySudoku("90");
+		/*if( mSudokus==null || mSudokus.length == 0 ){
+			mDbHelper.insertSudoku();
+			mSudokus = mDbHelper.querySudoku("A1");
+		}*/
+
+		titleBarLayout = findViewById(R.id.title_bar_layout);
+		downBarLayout = findViewById(R.id.down_bar_layout);
+		helpPanel = (Panel)findViewById(R.id.help_panel);
+		ratingBar = (RatingBar)findViewById(R.id.difficulty_ratingbar);
+		sudokuId = (TextView)findViewById(R.id.sudoku_id_textview);
+		
+		timerTextView = (TextView)findViewById(R.id.timer_textview);
+		closingTimeTextView = (TextView)findViewById(R.id.closing_time_textview);
+
+		mSudokuViewList = new ArrayList<View>();
+		mSquareViewList = new ArrayList<TextView[]>();
+
+		LayoutInflater layoutInflater = getLayoutInflater();
+
+		View view1 = layoutInflater.inflate(R.layout.sudoku_item9, null);
+		View view2 = layoutInflater.inflate(R.layout.sudoku_item9, null);
+		View view3 = layoutInflater.inflate(R.layout.sudoku_item9, null);
+		View view4 = layoutInflater.inflate(R.layout.sudoku_item9, null);
+
+		mSquareViewIds = new int[count];
+		mSquareViewIds[0] = R.id.Button0;
+		mSquareViewIds[1] = R.id.Button1;
+		mSquareViewIds[2] = R.id.Button2;
+		mSquareViewIds[3] = R.id.Button3;
+		mSquareViewIds[4] = R.id.Button4;
+		mSquareViewIds[5] = R.id.Button5;
+		mSquareViewIds[6] = R.id.Button6;
+		mSquareViewIds[7] = R.id.Button7;
+		mSquareViewIds[8] = R.id.Button8;
+		mSquareViewIds[9] = R.id.Button9;
+		mSquareViewIds[10] = R.id.Button10;
+		mSquareViewIds[11] = R.id.Button11;
+		mSquareViewIds[12] = R.id.Button12;
+		mSquareViewIds[13] = R.id.Button13;
+		mSquareViewIds[14] = R.id.Button14;
+		mSquareViewIds[15] = R.id.Button15;
+		mSquareViewIds[16] = R.id.Button16;
+		mSquareViewIds[17] = R.id.Button17;
+		mSquareViewIds[18] = R.id.Button18;
+		mSquareViewIds[19] = R.id.Button19;
+		mSquareViewIds[20] = R.id.Button20;
+		mSquareViewIds[21] = R.id.Button21;
+		mSquareViewIds[22] = R.id.Button22;
+		mSquareViewIds[23] = R.id.Button23;
+		mSquareViewIds[24] = R.id.Button24;
+		mSquareViewIds[25] = R.id.Button25;
+		mSquareViewIds[26] = R.id.Button26;
+		mSquareViewIds[27] = R.id.Button27;
+		mSquareViewIds[28] = R.id.Button28;
+		mSquareViewIds[29] = R.id.Button29;
+		mSquareViewIds[30] = R.id.Button30;
+		mSquareViewIds[31] = R.id.Button31;
+		mSquareViewIds[32] = R.id.Button32;
+		mSquareViewIds[33] = R.id.Button33;
+		mSquareViewIds[34] = R.id.Button34;
+		mSquareViewIds[35] = R.id.Button35;
+		mSquareViewIds[36] = R.id.Button36;
+		mSquareViewIds[37] = R.id.Button37;
+		mSquareViewIds[38] = R.id.Button38;
+		mSquareViewIds[39] = R.id.Button39;
+		mSquareViewIds[40] = R.id.Button40;
+		mSquareViewIds[41] = R.id.Button41;
+		mSquareViewIds[42] = R.id.Button42;
+		mSquareViewIds[43] = R.id.Button43;
+		mSquareViewIds[44] = R.id.Button44;
+		mSquareViewIds[45] = R.id.Button45;
+		mSquareViewIds[46] = R.id.Button46;
+		mSquareViewIds[47] = R.id.Button47;
+		mSquareViewIds[48] = R.id.Button48;
+		mSquareViewIds[49] = R.id.Button49;
+		mSquareViewIds[50] = R.id.Button50;
+		mSquareViewIds[51] = R.id.Button51;
+		mSquareViewIds[52] = R.id.Button52;
+		mSquareViewIds[53] = R.id.Button53;
+		mSquareViewIds[54] = R.id.Button54;
+		mSquareViewIds[55] = R.id.Button55;
+		mSquareViewIds[56] = R.id.Button56;
+		mSquareViewIds[57] = R.id.Button57;
+		mSquareViewIds[58] = R.id.Button58;
+		mSquareViewIds[59] = R.id.Button59;
+		mSquareViewIds[60] = R.id.Button60;
+		mSquareViewIds[61] = R.id.Button61;
+		mSquareViewIds[62] = R.id.Button62;
+		mSquareViewIds[63] = R.id.Button63;
+		mSquareViewIds[64] = R.id.Button64;
+		mSquareViewIds[65] = R.id.Button65;
+		mSquareViewIds[66] = R.id.Button66;
+		mSquareViewIds[67] = R.id.Button67;
+		mSquareViewIds[68] = R.id.Button68;
+		mSquareViewIds[69] = R.id.Button69;
+		mSquareViewIds[70] = R.id.Button70;
+		mSquareViewIds[71] = R.id.Button71;
+		mSquareViewIds[72] = R.id.Button72;
+		mSquareViewIds[73] = R.id.Button73;
+		mSquareViewIds[74] = R.id.Button74;
+		mSquareViewIds[75] = R.id.Button75;
+		mSquareViewIds[76] = R.id.Button76;
+		mSquareViewIds[77] = R.id.Button77;
+		mSquareViewIds[78] = R.id.Button78;
+		mSquareViewIds[79] = R.id.Button79;
+		mSquareViewIds[80] = R.id.Button80;
+
+		mSudokuViewList.add( view1 );
+		mSudokuViewList.add( view2 );
+		mSudokuViewList.add( view3 );
+		mSudokuViewList.add( view4 );
+
+		for (View view : mSudokuViewList) {
+			mSquaresViews = new TextView[count];
+			for(int i = 0; i < count; i++){
+				mSquaresViews[i] = ( TextView )view.findViewById(mSquareViewIds[i]);
+				mSquaresViews[i].setTag(R.id.number, i);
+			}
+			mSquareViewList.add(mSquaresViews);
+		}
+
+		mViewPager = (MyViewPager)findViewById(R.id.sudoku_viewpager);
+		mAdapter = new PlayPagerAdapter(mSudokus.length, mSudokuViewList);
+		mAdapter.setOnInstantiateItemListener(this);
+		mViewPager.setAdapter(mAdapter);
+		mViewPager.setOnPageChangeListener(this);
+		mViewPager.setOnTouchListener(this);
+		onPageSelected( 0 );
+
+		//对话框
+		mDialog = new Dialog(this, R.style.transparent_dialog);
+		mDialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		View view = getLayoutInflater().inflate(R.layout.dialog9, null);
+		mDialog.setContentView(view);
+
+		mDialogViewIds = new int[squarelength];
+		mDialogViewIds[0] = R.id.DialogButton0;
+		mDialogViewIds[1] = R.id.DialogButton1;
+		mDialogViewIds[2] = R.id.DialogButton2;
+		mDialogViewIds[3] = R.id.DialogButton3;
+		mDialogViewIds[4] = R.id.DialogButton4;
+		mDialogViewIds[5] = R.id.DialogButton5;
+		mDialogViewIds[6] = R.id.DialogButton6;
+		mDialogViewIds[7] = R.id.DialogButton7;
+		mDialogViewIds[8] = R.id.DialogButton8;
+		mDialogViews = new TextView[squarelength];
+		for(int i = 0; i < squarelength; i++){
+			mDialogViews[i] = ( TextView )view.findViewById(mDialogViewIds[i]);
+			mDialogViews[i].setTag(R.id.number, i);
+		}
+	}
+
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		Display display = getWindowManager().getDefaultDisplay();
+		width = display.getWidth(); 
+		height = display.getHeight();
+		super.onResume();
+	}
+
+	@Override
+	public void onBackPressed() {
+		// TODO Auto-generated method stub
+		if(helpPanel.isOpen()){
+			helpPanel.onClick();
+			return;
+		}
+
+		if(mPlaying){
+			mPlaying = false;
+			hideSystemUi(mPlaying);
+			mViewPager.setPlaying(mPlaying);
+			handler.removeCallbacks(runnable);
+			closingTimeTextView.setText(Calendar.getInstance().getTime().toString());
+			return;
+		}
+		super.onBackPressed();
+	}
+
+	public void onPageScrollStateChanged(int arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onPageScrolled(int arg0, float arg1, int arg2) {
+		// TODO Auto-generated method stub
+	}
+
+	public void onPageSelected(int arg0) {
+		// TODO Auto-generated method stub
+		mCurrentIndex = arg0;
+		float rating = mSudokus[arg0].degreeOfDifficulty;
+		ratingBar.setRating( rating );
+		sudokuId.setText( String.valueOf( mSudokus[arg0]._id ) );
+		mSquaresViews = mSquareViewList.get( arg0 % mSquareViewList.size() );
+		mData = mSudokus[arg0].data;
+		
+		timerTextView.setText("");
+		closingTimeTextView.setText("");
+		
+		Log.e("onPageSelected", ""+arg0+" "+rating+" "+String.valueOf(mSudokus[arg0].data));
+	}
+
+	public void onClick( View view ){
+		switch (view.getId()) {
+
+		case R.id.timer_button:
+			//开始计时
+			mPlaying = true;
+			hideSystemUi(mPlaying);
+			mViewPager.setPlaying(mPlaying);
+			recLen = 0;
+			handler.postDelayed(runnable, 1000);
+			break;
+
+		case R.id.help_button:
+			helpPanel.onClick();
+			break;
+
+		default:
+			if(mPlaying){
+				boolean blank = (Boolean) view.getTag(R.id.blank);
+				if(blank){
+					mClickView = view;
+					mClickNumber = (Integer) mClickView.getTag(R.id.number);
+
+					boolean[] have = getHave();
+					boolean b;
+					for (int i = 0; i < squarelength; i++ ) {
+						b = have[i];
+						mDialogViews[i].setTag( R.id.blank, b );
+						if(b){
+							mDialogViews[i].setTextColor(Color.WHITE);
+						}
+						else{
+							mDialogViews[i].setTextColor(getResources().getColor(R.color.purple));
+						}
+					}
+
+					int [] xy =  getXY(mClickView);
+					WindowManager.LayoutParams lp=mDialog.getWindow().getAttributes();
+					//lp.alpha=0.5f;
+					lp.x = xy[0];
+					lp.y = xy[1];
+					mDialog.getWindow().setAttributes(lp);
+					mDialog.show();
+				}
+			}
+			else{
+				Toast.makeText(this, "点击左下角按钮开始", Toast.LENGTH_SHORT).show();
+			}
+			break;
+		}
+	}
+
+	public void onDialogClick( View view ){
+		boolean blank = (Boolean) view.getTag(R.id.blank);
+		if( !blank ){
+			int value = (Integer) view.getTag(R.id.number);
+			value++;
+			((TextView)mClickView).setText( String.valueOf(value) );
+			mData[mClickNumber] = value;
+
+			int i;
+			for(i = 0; i < count && mData[i] != 0; i++){
+
+			}
+
+			if( i >= count ){
+				Toast.makeText(this, "success", Toast.LENGTH_LONG).show();
+				onBackPressed();
+			}
+
+			mDialog.dismiss();
+		}
+	}
+
+	/**
+	 * 是否隐藏状态栏和标题栏（即是否全屏）。
+	 * @param hide
+	 */
+	private void hideSystemUi(boolean hide) {
+		Window window = getWindow();
+		if (hide) {
+			titleBarLayout.setVisibility(View.GONE);
+			downBarLayout.setVisibility(View.GONE);
+			window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+					WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		} else {
+			window.setFlags(0, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+			titleBarLayout.setVisibility(View.VISIBLE);
+			downBarLayout.setVisibility(View.VISIBLE);
+		}
+	}
+
+	/**
+	 * 计算view中心位置与屏幕中心的偏移
+	 * @param view
+	 * @return
+	 */
+	public int[] getXY( View view ){
+		int x = view.getLeft() + view.getRight();
+		int y = view.getTop() + view.getBottom();
+
+		int []xy = new int[2];
+		xy[0] = (x - width) >> 1;
+					xy[1] = (y - height) >> 1;
+
+			return xy;
+	}
+
+	public boolean onTouch(View v, MotionEvent event) {
+		// TODO Auto-generated method stub
+		if(helpPanel.isOpen()){
+			helpPanel.onClick();
+			return true;
+		}
+
+		if( mPlaying ){
+			//不允许viewpager滑动
+			return true;
+		}
+		return false;
+	}
+
+	public boolean OnInstantiateItem(int index, int position) {
+		// TODO Auto-generated method stub
+		int[] values = mSudokus[position].data;
+		mSquaresViews = mSquareViewList.get(index);
+		for(int i = 0; i < mSquaresViews.length; i++){
+
+			int x = i % squarelength;
+			int y = i / squarelength;
+			
+			int xi = x / squarewidth * squarewidth;
+			int yi = y / squareheight * squareheight;
+			
+			int key = xi + yi * squarewidth;
+			
+			if( key % 2 == 0 ){
+				mSquaresViews[i].setBackgroundResource(R.drawable.purple_light);
+			}
+			else{
+				mSquaresViews[i].setBackgroundResource(R.drawable.purple_dark);
+			}
+
+			if( values[i] == 0 ){
+				mSquaresViews[i].setTextColor(getResources().getColor(R.color.purple));
+				mSquaresViews[i].setText( "" );
+				mSquaresViews[i].setTag(R.id.blank, true);
+			}
+			else{
+				mSquaresViews[i].setTextColor(Color.WHITE);
+				mSquaresViews[i].setText( String.valueOf( values[i] ) );
+				mSquaresViews[i].setTag(R.id.blank, false);
+			}
+		}
+		return false;
+	}
+
+	private boolean[] getHave(){
+		int x = mClickNumber % squarelength;
+		int y = mClickNumber / squarelength;
+
+		int xi = x / squarewidth * squarewidth;
+		int yi = y / squareheight * squareheight;
+
+		boolean []have = new boolean[squarelength];
+		for(int i = 0; i < squarelength; i++){
+			have[i] = false;
+		}
+
+		for(int i = 0, line = y * squarelength, row = x, block = yi * squarelength + xi; 
+				i < squarelength; 
+				i++, line++, row += squarelength, block++){
+			int value = mData[line];
+			if( value != 0 ){
+				have[value-1] = true;
+			}
+
+			value = mData[row];
+			if( value != 0 ){
+				have[value-1] = true;
+			}
+
+			if( i % squarewidth == 0 && i != 0){
+				block += ( squarewidth * 2 );
+			}
+			value = mData[block];
+			if( value != 0 ){
+				have[value-1] = true;
+			}
+		}
+		return have;
+	}
+}
